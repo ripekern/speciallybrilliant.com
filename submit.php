423ca76ad0811476598aea9c825c9c20281544f2
ripekern <swenbergman@protonmail.com>
2022-12-08 01:58:21 -0800

    wordpress

---
+++ submit.php
@@ -0,0 +1,23 @@
+<?php
+
+// Google reCAPTCHA API keys settings
+$secretKey = '6Ld3sWMjAAAAACNoVAFOMSt6fCCdNhpw23gT7XZU';
+
+// Email settings
+$recipientEmail = 'swenabergman@gmail.com';
+
+// Assign default values
+$postData = $valErr = $statusMsg = '';
+$status = 'error';
+
+// If the form is submitted
+if(isset($_POST['submit_frm'])){
+  // Get the submitted form data
+  $postData = $_POST;
+  $name = trim($_POST['data[name]']);
+  $phone = trim($_POST['data[phone]']);
+  $message = trim($_POST['data[message]']);
+}
+
+
+?>
